1a05cfdcbee7fde4635dc0e13a3f805770387772
DreamerHridoy <eng.sahidul.hridoy@gmail.com>
2024-11-21 15:41:24 +0600

    little step aheading

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,39 +1,36 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Slider = () => {
+  const slides = [
+    "https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp",
+    "https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp",
+    "https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp",
+  ];
+
   return (
     <div>
-      <div className="carousel w-full">
-        <div id="item1" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item2" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
-          />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
-          />
-        </div>
-      </div>
-      <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-      </div>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 3000 }}
+        loop
+        className="w-full h-64"
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={slide}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
